fbe2b772cc6e1557e63b481c2537de850c8431bf
Shahryar Ahmad <shehryarshahid49@gmail.com>
2025-01-09 00:01:39 +0500

    Update

--- nvim/lua/thegentleman/plugins/lspconfig.lua
+++ nvim/lua/thegentleman/plugins/lspconfig.lua
@@ -12,16 +12,7 @@ return {
         local protocol = require("vim.lsp.protocol")
 
         local on_attach = function(client, bufnr)
-            -- format on save
-            if client.server_capabilities.documentFormattingProvider then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                })
-            end
+           
         end
 
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
--- nvim/lua/thegentleman/plugins/mason.lua
+++ nvim/lua/thegentleman/plugins/mason.lua
@@ -18,13 +18,9 @@ return {
 
         require("mason-tool-installer").setup({
             ensure_installed = {
-                "prettier",
-                "stylua", -- lua formatter
-                "isort", -- python formatter
-                "black", -- python formatter
-                "pylint",
-                "eslint_d",
-                "clangd"
+                "clangd",
+		"pyright",
+		"jsonls"
             },
         })
     end,
